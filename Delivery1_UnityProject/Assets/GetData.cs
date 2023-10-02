66fdbb6b80a2ced85676696b47463aa7fbb444e0
Xavierlm11 <xaviloma11@gmail.com>
2023-10-02 13:13:49 +0200

    albert mama56h

---
+++ Delivery1_UnityProject/Assets/GetData.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Simulator.OnNewPlayer += MyMethod;
+    }
+
+    private void MyMethod(string arg1, string arg2, DateTime time)
+    {
+        throw new NotImplementedException();
+    }
+
+  
+}
