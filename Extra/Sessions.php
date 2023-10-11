8c8872f8bf1ea5ecc7e61aebad84129566dd4cb9
Xavier Casadó <79162780+Akage369@users.noreply.github.com>
2023-10-11 11:07:28 +0200

    There is nothing we can do

--- Extra/Sessions.php
+++ Extra/Sessions.php
@@ -17,11 +17,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $playerCountry = $_POST["playerCountry"];
     $signUpTime = $_POST["signUpTime"];
 
+    echo "Bombardeen Lideraje\n";
+
+    $servername = "localhost";
+    $username = "xaviercb12";
+    $password = "8QGQefMvS38H";
+    $dbname = "xaviercb12";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+        echo "Connection failed";
+        return;
+    }
+
+    echo "Connection done correctly";
+    
+
+    $sql = "INSERT INTO Users ( `User_Name`, `User_Age`, `User_Gender`, `User_Country`, `Sign_Up_Time`) VALUES ("lolo",23,"F","Spain",'1998-01-23 12:45:56')";
+
+    $conn->close();
     // Realizar acciones con los datos, por ejemplo, guardarlos en una base de datos
     // o realizar algún otro procesamiento
 
     // Enviar una respuesta de vuelta a Unity
-    echo "Bombardeen Lideraje";
+    
 } else {
     echo "Método no permitido";
 }
