c2ded033dfc70880d8a4806deef886b333cd344c
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2023-11-04 17:05:22 +0100

    Reestructura y simplificacion de lo que tenemos
    @Akage369 Suda de los JSON que con un echo ya va. No nos hacen faltan echos cada 2 segundos de q todo está bien asi que usalos solo para logear errores y pasar variables

--- Delivery1_UnityProject/Assets/Scripts/GetData.cs
+++ Delivery1_UnityProject/Assets/Scripts/GetData.cs
@@ -1,32 +1,20 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class GetData : MonoBehaviour
 {
-    //https://docs.unity3d.com/es/530/Manual/UnityWebRequest.html
-
     [SerializeField] string phpUrl = "https://citmalumnes.upc.es/~xaviercb12/Sessions.php";
 
-    [SerializeField] string temporalID;
+    [SerializeField] int temporalID;
 
     void OnEnable()
     {
         Simulator.OnNewPlayer += Simulator_OnNewPlayer;
         Simulator.OnNewSession += Simulator_OnNewSession;
         Simulator.OnEndSession += Simulator_OnEndSession;
-
-        //Simulator.GetPlayerID += GetPlayerID;
     }
-
-    //private void GetPlayerID()
-    //{
-    //    StartCoroutine(ObtainUserID());
-    //}
 
     void OnDisable()
     {
@@ -39,7 +27,7 @@ public class GetData : MonoBehaviour
     {
         StartCoroutine(SendPlayerData(playerName, playerAge, playerGender, playerCountry, signUpTime));
     }
-    
+
     private IEnumerator SendPlayerData(string playerName, int playerAge, string playerGender, string playerCountry, DateTime signUpTime)
     {
         WWWForm form = new WWWForm();
@@ -52,61 +40,21 @@ public class GetData : MonoBehaviour
         form.AddField("playerCountry", playerCountry);
         form.AddField("signUpTime", signUpTime.ToString("yyyy-MM-dd HH:mm:ss"));
 
-        yield return SendToPHP(form);
+        UnityWebRequest www = UnityWebRequest.Post(phpUrl, form);
 
-    }
+        yield return www.SendWebRequest();
 
-    private IEnumerator SendToPHP(WWWForm form) 
-    {
-        using (UnityWebRequest www = UnityWebRequest.Post(phpUrl, form))
+        if (www.result != UnityWebRequest.Result.Success)
         {
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log("Error: " + www.error);
-            }
-            else
-            {
-                Debug.Log("Form upload complete!");
-
-                string response = www.downloadHandler.text;
-
-                MyData data = JsonUtility.FromJson<MyData>(response);
-                Debug.Log(data.userId);
-                // if(!Regex.IsMatch(www.downloadHandler.text, @"PHP:")){
-                //    temporalID = www.downloadHandler.text;
-                //}
-                //Debug.Log(temporalID);
-            }
+            Debug.Log("Error: " + www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+            Debug.Log(www.downloadHandler.text);
+            temporalID = int.Parse(www.downloadHandler.text);
         }
     }
-
-    //IEnumerator ObtainUserID()
-    //{
-    //    WWWForm form = new WWWForm();
-
-    //    form.AddField("methodToCall", "GetPlayerID");
-
-    //    using (UnityWebRequest www = UnityWebRequest.Get(phpUrl))
-    //    {
-    //        yield return www.SendWebRequest();
-
-    //        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
-    //        {
-    //            Debug.Log("Error: " + www.error);
-    //        }
-    //        else
-    //        {
-    //            string jsonResult = www.downloadHandler.text;
-    //            // Analiza el JSON para obtener el valor
-    //            var result = JsonUtility.FromJson<int>(jsonResult);
-    //            Debug.Log("Valor: " + result);
-    //        }
-    //    }
-
-    //    yield return null;
-    //}
 
     private void Simulator_OnNewSession(DateTime time)
     {
--- Extra/Sessions.php
+++ Extra/Sessions.php
@@ -1,12 +1,7 @@
 <?php
-/**
- * Export to PHP Array plugin for PHPMyAdmin
- * @version 5.0.4deb2
- */
 
-/**
- * Database `xaviercb12`
- */
+// He simplificado todo a lo basico, suda de los JSON cuando se puede hacer un echo de la variable. 
+// NO USAR ECHO para debug, solo para errores
 
  $servername = "localhost";
  $username = "xaviercb12";
@@ -15,24 +10,54 @@
 
  $conn;
 
-$debugMessages = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
- function ConnectToServer() 
-{
     global $debugMessages;
+    
+    $debugMessages = "";
 
+    CreatePlayer();
+    
+} else {
+    echo "PHP: Método no permitido \n";
+}
+
+function CreatePlayer() {
+
+    // Acceder a los datos enviados desde Unity
+    $playerName = $_POST["playerName"];
+    $playerAge = $_POST["playerAge"];
+    $playerGender = $_POST["playerGender"];
+    $playerCountry = $_POST["playerCountry"];
+    $signUpTime = $_POST["signUpTime"];
+
+    global $conn;
+
+    if(ConnectToServer() == false){
+        return;
+    }
+
+    $sql = "INSERT INTO Users ( User_Name, User_Age, User_Gender, User_Country, Sign_Up_Time) VALUES ('$playerName',$playerAge,'$playerGender','$playerCountry','$signUpTime')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo $conn->insert_id;
+    } else {
+        echo "PHP: Error al insertar datos: " . mysqli_error($conn);
+    }
+
+    CloseConnection();
+}
+
+function ConnectToServer() 
+{
     global $servername, $username, $password, $dbname;
     global $conn;
     
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
-        $debugMessages .= "Conexión fallida: " . $conn->connect_error;
-        die($debugMessages);
+        die("Conexión fallida: " . $conn->connect_error);
     }
-
-    $debugMessages .= "Connection done correctly\n";
-    //echo $debugMessages;
 
     return true;
 }
@@ -43,87 +68,4 @@ function CloseConnection()
     $conn->close();
 }
 
-/* `xaviercb12`.`Sessions` */
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    global $debugMessages;
-    
-    $debugMessages = "";
-
-    $methodToCall = $_POST["methodToCall"];
-
-    switch($methodToCall){
-        case "CreatePlayer":
-            CreatePlayer();
-            break;
-
-        case "GetPlayerID":
-            GetPlayerID();
-            break;
-    }
-
-    // Realizar acciones con los datos, por ejemplo, guardarlos en una base de datos
-    // o realizar algún otro procesamiento
-
-    // Enviar una respuesta de vuelta a Unity
-    
-} else {
-    $debugMessages .= "PHP: Método no permitido \n";
-    //echo $debugMessages;
-}
-
-function CreatePlayer() {
-    // Acceder a los datos enviados desde Unity
-    $playerName = $_POST["playerName"];
-    $playerAge = $_POST["playerAge"];
-    $playerGender = $_POST["playerGender"];
-    $playerCountry = $_POST["playerCountry"];
-    $signUpTime = $_POST["signUpTime"];
-
-    global $conn;
-    global $debugMessages;
-
-    if(ConnectToServer() == false){
-        return;
-    }
-
-    $sql = "INSERT INTO Users ( User_Name, User_Age, User_Gender, User_Country, Sign_Up_Time) VALUES ('$playerName',$playerAge,'$playerGender','$playerCountry','$signUpTime')";
-
-    if (mysqli_query($conn, $sql)) {
-        $debugMessages .= "PHP: Datos insertados con éxito. User ID: " . $conn->insert_id . "\n";
-
-        $dataToReturn = array(
-            'debugMessages' => $debugMessages,
-            'userId' => $conn->insert_id,
-        );
-        
-        echo json_encode($dataToReturn);
-    } else {
-        $debugMessages .= "PHP: Error al insertar datos: " . mysqli_error($conn) + "\n";
-        //echo $debugMessages;
-    }
-
-    CloseConnection();
-}
-
-function GetPlayerID() {
-
-    global $conn;
-
-    if(!ConnectToServer()){
-        return;
-    }
-    
-    $sql = "SELECT * FROM Users WHERE User_Id = (SELECT MAX(User_Id) FROM Users)";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $valor = $row["columna_deseada"];
-        echo json_encode(array("valor" => $valor));
-    } else {
-        echo json_encode(array("error" => "No se encontraron resultados"));
-    }
-
-    CloseConnection();
-}
+?>
