7d94cb28fde7a0b36e03cf94a9c5c30ab75b701b
Xavierlm11 <xaviloma11@gmail.com>
2023-11-06 20:36:52 +0100

    dates

---
+++ Extra/ScriptFunctions.sql
@@ -0,0 +1,28 @@
+
+### SESSIONS
+	# nº Sessions
+
+
+	# Avg lenght of Sesssion
+
+### MONETISATION
+
+	#ARPU
+    
+    #ARRPU
+
+### NUMBER OF USERS
+	
+    #DAU
+    
+    #MAU
+
+### RETENTION
+
+	#D1
+    
+    #D7
+    
+    #DX1
+    
+    #DX2
