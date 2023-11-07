8de8cdad3333dff8476050d4261d084cb3bb1f90
Xavierlm11 <xaviloma11@gmail.com>
2023-11-07 01:20:57 +0100

    Update ScriptFunctions.sql

--- Extra/ScriptFunctions.sql
+++ Extra/ScriptFunctions.sql
@@ -1,9 +1,19 @@
 
 ### SESSIONS
 	# nº Sessions
-
-
+    /*
+select count(s.session_id) AS "Nº of Sessions"
+from xaviercb12.Users u
+left join xaviercb12.Sessions s
+on u.User_Id = s.User_Id	
+*/
 	# Avg lenght of Sesssion
+select  u.User_Country, avg(timestampdiff(minute,s.Start_Timestamp, s.End_Timestamp)) AS "AVG time of Session per Country"
+from xaviercb12.Users u
+right join xaviercb12.Sessions s
+on u.User_Id = s.User_Id	
+group by u.User_Country
+
 
 ### MONETISATION
 
@@ -42,14 +52,32 @@ on d.date = m.date
 GROUP BY d.date
 */
 	#D1
-    /*
-select count(distinct s.player_id)/ count(distinct u.user_id)
+  /*  
+select count(distinct s.User_Id)/ count(distinct u.User_Id) AS D1
+from xaviercb12.Users u 
+left join xaviercb12.Sessions s
+on u.User_Id = s.User_Id  and DATE(s.Start_Timestamp - interval 1 day) =  date(u.Sign_Up_Time)  ;
+*/
+
+
+    #D7
+     /* 
+select count(distinct s.player_id)/ count(distinct u.user_id) AS D7
 from test.users u 
 left join test.sessions s
-on u.user_id = s.player_id  and DATE(s.start - interval 7 day) = u.dateCreated
+on u.user_id = s.player_id  and DATE(s.start - interval 7 day) = u.dateCreated;
 */
-    #D7
-    
-    #DX1
-    
-    #DX2
+    #D13
+      /*
+select count(distinct s.player_id)/ count(distinct u.user_id) AS D13
+from test.users u 
+left join test.sessions s
+on u.user_id = s.player_id  and DATE(s.start - interval 13 day) = u.dateCreated;
+*/
+    #D56
+      /*
+select count(distinct s.player_id)/ count(distinct u.user_id) AS D17
+from test.users u 
+left join test.sessions s
+on u.user_id = s.player_id  and DATE(s.start - interval 56 day) = u.dateCreated;
+*/
