2ae2b555b8e864b5232d58fc22aac78aba1e0526
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2024-01-21 22:54:13 +0100

    Canvas update

--- Delivery3_UnityProj/Assets/CanvasScript.cs
+++ Delivery3_UnityProj/Assets/CanvasScript.cs
@@ -1,17 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasScript : MonoBehaviour
 {
+    [SerializeField] Dropdown playersDropdown;
+    List<string> playerIds = new();
+
     void Start()
     {
-        
+        // Igualar playerIds a todos los userIds o sessionId que haya (Seguramente haya q pasar ints a string)
+
+        playersDropdown.AddOptions(playerIds);
     }
 
     void Update()
     {
         
+    }
+
+    public void DropDown_SetEvent(int eventId)
+    {
+
+    }
+
+    public void DropDown_SetPlayer(int playerId)
+    {
+
+    }
+
+    public void Button_GetData()
+    {
+
+    }
+
+    public void Button_StartHeatmap()
+    {
+
+    }
+
+    public void Button_Restart()
+    {
+
     }
 
     public void SetOpacity(float opacity)
