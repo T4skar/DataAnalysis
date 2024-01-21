af8dc0a8da5a85d0acfe44307bfb74a325e014a8
Xavierlm11 <xaviloma11@gmail.com>
2024-01-21 18:04:31 +0100

    Update HeatMapGenerator.cs

--- Delivery3_UnityProj/Assets/HeatMapGenerator.cs
+++ Delivery3_UnityProj/Assets/HeatMapGenerator.cs
@@ -7,33 +7,33 @@ using UnityEngine;
 public class HeatMapGenerator : MonoBehaviour
 {
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         try
         {
+            string jsonPath = "PlayerPathJson";
+            string txtPath = "PlayerPathTxt";
 
-            //SerializeJson();
-            //SerializeJsonList();
-            //SerializeJsonList2();
-            SerializeJsonList3();
+            SerializeJsonList3(dataarray, jsonPath);
+            JsonToTxtConverter(jsonPath, txtPath);
             Debug.Log("Ha FUNCIONADO el escribir en JASON");
         }
-        catch 
+        catch
 
         {
             Debug.Log("Ha fallado el escribir en JASON");
         }
     }
 
-    
+
 
     //[SerializeField]
     //public List<TestClass> testlist = new List<TestClass>();
 
-    public TestClassList testlist= new TestClassList();
+    public TestClassList testlist = new TestClassList();
     public TestClass[] dataarray = new TestClass[]
     {
         new TestClass{Position=new Vector3(33.23f,23,232.333f),EventName="Object Position"},
@@ -42,67 +42,14 @@ public class HeatMapGenerator : MonoBehaviour
     };
 
     public TestClasslis2 testClasslis2 = new TestClasslis2();
-    void SerializeJson()
-    {
-        string filePath = "Assets/playerData2.json";
-        var t = new TestClass();
-        string a;
-       
-        t.Position =  new Vector3(12.123f, 23.2f, 333.3f);
-        t.EventName = "Object Position";
-       // a = "\"Position\":{\"x\":" + t.posX.ToString() + ",\"y\":" + t.posY.ToString() + ",\"z\":" + t.posX.ToString() + "},\"EventName\":\"Object Position\"";
-        string json = JsonUtility.ToJson(t);
-        Stream stream = new MemoryStream();
-        //BinaryWriter writer = new BinaryWriter(stream);
-        //writer.Write(json);
-        StreamWriter writer2 = new StreamWriter("Assets/playerData2.json", true);
-        writer2.Write(json);
-        // Path to save the JSON file
 
-        // Write the JSON string to a text file
-        File.WriteAllText(filePath,json);
-        
-       // Debug.Log("JSON data saved to: " + filePath);
-    }
-    void SerializeJsonList()
+    void SerializeJsonList3(TestClass[] itemJson, string pathjson)
     {
-        
-       // a = "\"Position\":{\"x\":" + t.posX.ToString() + ",\"y\":" + t.posY.ToString() + ",\"z\":" + t.posX.ToString() + "},\"EventName\":\"Object Position\"";
-        string json = JsonUtility.ToJson(testlist);
-        Stream stream = new MemoryStream();
-        BinaryWriter writer = new BinaryWriter(stream);
-        writer.Write(json);
-        
-        // Path to save the JSON file
-        string filePath = /*Application.persistentDataPath +*/ "Assets/playerDataList.json";
 
-        // Write the JSON string to a text file
-        File.WriteAllText(filePath,json);
-        
-        Debug.Log("JSON data saved to: " + filePath);
-    }
-    void SerializeJsonList2()
-    {
-        
-       // a = "\"Position\":{\"x\":" + t.posX.ToString() + ",\"y\":" + t.posY.ToString() + ",\"z\":" + t.posX.ToString() + "},\"EventName\":\"Object Position\"";
-        string json = JsonUtility.ToJson(testClasslis2);
-        Stream stream = new MemoryStream();
-        BinaryWriter writer = new BinaryWriter(stream);
-        writer.Write(json);
-        
-        // Path to save the JSON file
-        string filePath = /*Application.persistentDataPath +*/ "Assets/playerDataList4.json";
+        string filePathJson = Application.dataPath + "/" + pathjson + ".json";
 
-        // Write the JSON string to a text file
-        File.WriteAllText(filePath,json);
-        
-        Debug.Log("JSON data saved to: " + filePath);
-    }
-    void SerializeJsonList3()
-    {
-        
-        string filePath = Application.dataPath + "/playerData2.json";
-        using (StreamWriter writer = new StreamWriter(filePath))
+
+        using (StreamWriter writer = new StreamWriter(filePathJson))
         {
             foreach (TestClass item in dataarray)
             {
@@ -112,16 +59,82 @@ public class HeatMapGenerator : MonoBehaviour
                 writer.WriteLine(json);
             }
         }
-        //Stream stream = new MemoryStream();
-        
+
+
+        Debug.Log("JSON data saved to: " + filePathJson);
+    }
+
+    private static void JsonToTxtConverter(string PathJson, string PathTxt)
+    {
+
+        string filePathJson = Application.dataPath + "/" + PathJson + ".json";
+        string filePathTxt = Application.dataPath + "/" + PathTxt + ".json";
+
+        // Cargar el contenido del archivo JSON como una cadena
+        string jsonContent = File.ReadAllText(filePathJson);
+
+        // Guardar la cadena JSON como un archivo de texto
+        File.WriteAllText(filePathTxt, jsonContent);
+        Debug.Log("TXT data saved to: " + filePathTxt);
+    }
+
+    void SerializeJson()
+    {
+        string filePath = "Assets/playerData2.json";
+        var t = new TestClass();
+        string a;
+
+        t.Position = new Vector3(12.123f, 23.2f, 333.3f);
+        t.EventName = "Object Position";
+        // a = "\"Position\":{\"x\":" + t.posX.ToString() + ",\"y\":" + t.posY.ToString() + ",\"z\":" + t.posX.ToString() + "},\"EventName\":\"Object Position\"";
+        string json = JsonUtility.ToJson(t);
+        Stream stream = new MemoryStream();
+        //BinaryWriter writer = new BinaryWriter(stream);
+        //writer.Write(json);
+        StreamWriter writer2 = new StreamWriter("Assets/playerData2.json", true);
+        writer2.Write(json);
         // Path to save the JSON file
-        //string filePath = /*Application.persistentDataPath +*/ "Assets/playerDataList4.json";
 
         // Write the JSON string to a text file
-        //File.WriteAllText(filePath,json);
-        
-        //Debug.Log("JSON data saved to: " + filePath);
+        File.WriteAllText(filePath, json);
+
+        // Debug.Log("JSON data saved to: " + filePath);
     }
+    void SerializeJsonList()
+    {
+
+        // a = "\"Position\":{\"x\":" + t.posX.ToString() + ",\"y\":" + t.posY.ToString() + ",\"z\":" + t.posX.ToString() + "},\"EventName\":\"Object Position\"";
+        string json = JsonUtility.ToJson(testlist);
+        Stream stream = new MemoryStream();
+        BinaryWriter writer = new BinaryWriter(stream);
+        writer.Write(json);
+
+        // Path to save the JSON file
+        string filePath = /*Application.persistentDataPath +*/ "Assets/playerDataList.json";
+
+        // Write the JSON string to a text file
+        File.WriteAllText(filePath, json);
+
+        Debug.Log("JSON data saved to: " + filePath);
+    }
+    void SerializeJsonList2()
+    {
+
+        // a = "\"Position\":{\"x\":" + t.posX.ToString() + ",\"y\":" + t.posY.ToString() + ",\"z\":" + t.posX.ToString() + "},\"EventName\":\"Object Position\"";
+        string json = JsonUtility.ToJson(testClasslis2);
+        Stream stream = new MemoryStream();
+        BinaryWriter writer = new BinaryWriter(stream);
+        writer.Write(json);
+
+        // Path to save the JSON file
+        string filePath = /*Application.persistentDataPath +*/ "Assets/playerDataList4.json";
+
+        // Write the JSON string to a text file
+        File.WriteAllText(filePath, json);
+
+        Debug.Log("JSON data saved to: " + filePath);
+    }
+
 
 }
 [System.Serializable]
